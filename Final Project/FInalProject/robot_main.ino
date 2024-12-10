2efca6330346ebfa1ede449f1557432565858616
Grant Goldman <grantgoldman20@gmail.com>
2024-12-09 21:58:03 -0800

    Update robot_main.ino

--- Final Project/FInalProject/robot_main.ino
+++ Final Project/FInalProject/robot_main.ino
@@ -7,13 +7,16 @@
 #define coinDist 10 // in cm
 #define backdist 10 // in cm
 #define minChasis 10 // in cm
-#define backButton 5 // in cm
+#define backButton 56 // in cm
 #define greenAngle -90
 #define blueAngle -60
 #define whiteAngle -30
 #define redAngle 30
 #define purpleAngle 60
 #define yellowAngle 90
+#define servoDown 30
+#define ServoUp 60
+
 
 
 Servo myServo;
@@ -26,7 +29,9 @@ bool stage5 = false;
 bool stage6 = false;
 bool stage7 = false;
 bool stage8 = false;
-bool foundLine = false
+bool foundLine = false;
+float angleToTurn = 0;
+int angles [] = {-90, -60, -30, 30, 60, 90};
 
 
 
@@ -69,7 +74,7 @@ void loop() {
 
     //follow line until dist reads 10-20cm vars needed currentdist coindist stage3 stage4
     if(stage1==false && stage2==false && stage3==true && stage4==false && stage5==false && stage6==false && stage7==false && stage8==false){
-        currentDist=readDistance();
+        float currentDist=readDistance();
         if(currendDist<=coinDist){
           stage3=false;
           stage4=true;
@@ -88,7 +93,7 @@ void loop() {
 
     //go forward until distance reads min chassis dist to make sure pushed button vars needed minchasis stage5 stage6
       if(stage1==false && stage2==false && stage3==false && stage4==false && stage5==true && stage6==false && stage7==false && stage8==false){
-          currentDist=readDistance();
+          float currentDist=readDistance();
           if(currentDist <= minChasis){
             Forward();
           }
@@ -117,6 +122,38 @@ void loop() {
     //reset angle
     //loop back with new color
     if(stage1==false && stage2==false && stage3==false && stage4==false && stage5==false && stage6==false && stage7==true ){
+      int color = //read color func
+      int angleToTurn = angles[color];
+      int reverse = angleToTurn * -1;
+      //turn to angle
+      float currentDist= readDistance()
+      while(currentDist> minChasis){
+        //forwardLine
+      }
+      myServo.write(servoDown);
+      delay(1000);
+      myServo.write(servoUp);
+      bool foundColor = false;
+      while(!foundColor){
+        //readColor1
+        //delay 50
+        //readColor2
+        //delay 50
+        //readColor3
+        //delay 50
+        //readColor4
+        //delay 50
+        //readColor5
+        //delay 50
+        //if (readColor1==readColor2==readColor3==readColor4==readColor5){
+          // foundColor=true
+          // color = whatever was found
+       // }
+        
+      }
+      cmReverse(backButton);
+      //turn to angle reverse
+
 
     }
 
